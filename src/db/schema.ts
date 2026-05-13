3513938f2f6a87b024a3b510b54847e2176f9937
Omar Jabour <jabour.omar@hotmail.com>
2026-05-13 21:20:33 +0200

    :closed_lock_with_key: modify schema.ts  ./.  :closed_lock_with_key:

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,9 +1,11 @@
 import { pgEnum, pgTable, uuid } from 'drizzle-orm/pg-core';
+import { text } from 'stream/consumers';
 
 export const userRoleEnum = pgEnum('user_role', ['admin', 'user']);
 
 export const users = pgTable('users', {
     id: uuid('id').primaryKey().defaultRandom(),
+    email: text('email').notNull().unique(),
 });
 
 
