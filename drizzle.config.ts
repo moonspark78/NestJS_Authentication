f848575618ea79701d85a1a6c6612c86e1331b0e
Omar Jabour <jabour.omar@hotmail.com>
2026-05-15 14:52:33 +0200

    :construction:  modify on config  ./././.  :construction:

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,6 +4,9 @@ export default defineConfig({
     schema: "./src/db/schema.ts",
     out: "./drizzle",
     dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+    }
 })
 
 
