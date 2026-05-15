d082a7c9874a8249b46826654d2475ea5dfeba7c
Omar Jabour <jabour.omar@hotmail.com>
2026-05-15 14:41:22 +0200

    :construction:  modify schema.ts & index.ts .  :construction:

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,9 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
+import * as schema from "./schema";
 
 
 const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql, { schema });
 
 
