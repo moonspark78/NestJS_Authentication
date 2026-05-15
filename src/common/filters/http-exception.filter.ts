eec9f989e403e13b3b64c9de7e1fac2f4781879e
Omar Jabour <jabour.omar@hotmail.com>
2026-05-15 15:10:45 +0200

    :construction:  modify on config  ././.  :construction:

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -4,5 +4,9 @@ import type {
 } from "@nestjs/common";
 
 export class HttpExceptionFilter implements ExceptionFilter {
-    catch(exception: unknown, host: ArgumentsHost) {}
+    catch(exception: unknown, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const request = ctx.getRequest<Request>();
+    }
 }
