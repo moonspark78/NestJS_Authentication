864f49c58321c3763e169cb1b2238062647fcb54
Omar Jabour <jabour.omar@hotmail.com>
2026-05-16 13:37:04 +0200

    :construction:  modify on main.ts ./  :construction:

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config'; 
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.use(cookieParser())
 
 
   await app.listen(process.env.PORT ?? 3000);
