61159537dd3b4ad8b07f9eb234e09a60e103cb33
Omar Jabour <jabour.omar@hotmail.com>
2026-05-16 13:36:22 +0200

    :construction:  modify on main.ts ./.  :construction:

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
