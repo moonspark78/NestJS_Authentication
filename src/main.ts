37aef8a1f92d2c4889a78bc89d9566781893967a
Omar Jabour <jabour.omar@hotmail.com>
2026-05-16 13:34:58 +0200

    :construction:  modify on main.ts ././  :construction:

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config'; 
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
