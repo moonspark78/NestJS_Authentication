357de19ae8ace0051a52c3daa5aa9df0f3b731af
Omar Jabour <jabour.omar@hotmail.com>
2026-05-16 13:37:30 +0200

    modify on main.ts .

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.use(cookieParser())
+  app.setGlobalPrefix('api');
 
 
   await app.listen(process.env.PORT ?? 3000);
