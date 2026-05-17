68e01c96a58b94594af62a1a132fe0677b5997fd
Omar Jabour <jabour.omar@hotmail.com>
2026-05-17 14:23:55 +0200

    create and working on users.services.ts ././././

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,2 +1,5 @@
 import { Injectable } from "@nestjs/common";
 import { db } from "src/db";
+import { users } from "src/db/schema";
+import { eq } from "drizzle-orm";
+import { NewUser } from "src/db/schema";
