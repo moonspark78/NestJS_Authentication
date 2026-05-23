5b039c6c2d96dcc8f369a1c994cc40db30512171
Omar Jabour <jabour.omar@hotmail.com>
2026-05-23 13:22:30 +0200

    working usersModule.ts ./././

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,1 +1,9 @@
-export class RegisterDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+
+
+export class RegisterDto {
+    @ApiProperty({ example: "John Doe" })
+    @IsString()
+    name!: string;
+}
