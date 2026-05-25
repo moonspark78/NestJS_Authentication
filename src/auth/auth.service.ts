613b5a88712f8937461923d39e70a8b4c9ba934a
Omar Jabour <jabour.omar@hotmail.com>
2026-05-25 14:03:48 +0200

    :tada: working on auth.service.ts  .././././././ :tada:

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,4 +5,7 @@ import {
     BadRequestException,
     NotFoundException,
 } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import {ConfigService} from "@nestjs/config";
+import * as bcrypt from "bcryptjs";
 
