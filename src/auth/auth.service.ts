b71be525d0ef1d7bb11326b200053c04ec31d015
Omar Jabour <jabour.omar@hotmail.com>
2026-05-25 14:04:50 +0200

    :tada: working on auth.service.ts  ../././././. :tada:

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,4 +8,7 @@ import {
 import { JwtService } from "@nestjs/jwt";
 import {ConfigService} from "@nestjs/config";
 import * as bcrypt from "bcryptjs";
+import * as crypto from "crypto";
+import { UsersService } from "src/users/users.service";
+import { EmailService } from "./email.service";
 
