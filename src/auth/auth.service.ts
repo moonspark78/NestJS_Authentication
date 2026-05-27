a5f4ec9d51a22b634a13cab138fdbe55c4646fa2
Omar Jabour <jabour.omar@hotmail.com>
2026-05-27 23:53:06 +0200

    :art:  working on the auth.services.ts /./././  :art:

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -114,6 +114,7 @@ export class AuthService {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
             sameSite: "lax",
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
     }
 }
