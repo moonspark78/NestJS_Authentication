cc7ae23b7f0797a273b8f3f0c7fbf28cec97f615
Omar Jabour <jabour.omar@hotmail.com>
2026-05-31 00:43:53 +0200

    :art:  working on the auth.CONTROLLER.ts  /./././././././  :art:

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,4 +9,10 @@ import {
     HttpCode,
     HttpStatus,
 } from '@nestjs/common'
-import { ApiTags, ApiOperation } from '@nestjs/swagger'
+import { ApiTags, ApiOperation, ApiCookieAuth, ApiBearerAuth } from '@nestjs/swagger'
+import { AuthService } from './auth.service'
+import { RegisterDto } from './dto/register.dto'
+import { LoginDto } from './dto/login.dto'
+import { Response, Request } from 'express'
+import type { User } from 'src/db/schema'
+import { Public } from '../common/decorators/current-user.decorator'
