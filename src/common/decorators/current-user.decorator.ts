ba70f8752b0e9e8ca7734d6373ed932cc99ad56a
Omar Jabour <jabour.omar@hotmail.com>
2026-05-31 00:49:02 +0200

    :art:  working on the auth.CONTROLLER.ts  /./././././.  :art:

--- src/common/decorators/current-user.decorator.ts
+++ src/common/decorators/current-user.decorator.ts
@@ -1,2 +1,5 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from 'src/db/schema';
+import { Request } from 'express';
+
+type RequestWithUser = Request & { user: User };
