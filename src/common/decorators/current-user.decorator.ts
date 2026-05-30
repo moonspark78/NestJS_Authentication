b98279349c6811a133d453c11c79a14a000e279f
Omar Jabour <jabour.omar@hotmail.com>
2026-05-31 00:50:10 +0200

    :art:  working on the auth.CONTROLLER.ts  /././././.  :art:

--- src/common/decorators/current-user.decorator.ts
+++ src/common/decorators/current-user.decorator.ts
@@ -5,5 +5,8 @@ import { Request } from 'express';
 type RequestWithUser = Request & { user: User };
 
 export const CurrentUser = createParamDecorator(
-    (_data: unknown, ctx: ExecutionContext) => {};
+    (_data: unknown, ctx: ExecutionContext) => {
+        const request = ctx.switchToHttp().getRequest<RequestWithUser>();
+        return request.user;
+    }
 );
