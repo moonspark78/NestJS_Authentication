bea06cd23c2c4f068b7e366929cbfef90f900082
Omar Jabour <jabour.omar@hotmail.com>
2026-06-04 11:06:46 +0200

    :art:  working on the auth.MODULE.ts /   :art:

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,3 +4,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { EmailService } from './email.service';
 import { UsersModule } from 'src/users/users.module';
+
+@Module({
+    imports: [UsersModule, JwtModule.register({})],
+})
