a0e5f15adf040a84cdbc58809f960c111fefbf47
Omar Jabour <jabour.omar@hotmail.com>
2026-06-05 11:36:09 +0200

    :art:  working on the jwt-auth-guard.ts   :art:

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
 
 
 @Module({
@@ -13,5 +14,6 @@ import { UsersModule } from './users/users.module';
     UsersModule,
     AuthModule,
   ],
+  providers: [{ provide: 'APP_GUARD', useClass: JwtAuthGuard }],
 })
 export class AppModule {}
